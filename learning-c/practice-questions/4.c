f71dc960bbff01870e3e63ca783e53b7e74dd80a
Devansh Soni <devanshsoni899@gmail.com>
2023-12-10 22:43:33 +0530

    practice-questiion 4 fastest way to calc gcd

--- learning-c/practice-questions/4.c
+++ learning-c/practice-questions/4.c
@@ -2,32 +2,28 @@
 /*
 * https://codeforces.com/problemset/problem/1041/B
 */
+long int gcd (long int a,long int b) ;
+
 int main(){
     long int a,b,x,y,count=0;
     scanf("%ld %ld %ld %ld",&a,&b,&x,&y);
-
-    // for(int i=2;;i++){
-
-    //     if(x%i==0&&y%i==0){
-    //         x/=i;
-    //         y/=i;
-    //         i--;
-    //     }
-    //     if(i>(x/i))
-    //         break;
-    // }
-    long int gcd;
-    for(int i=0;i<=x&&i<=y;i++){
-        if(x%i==0 && y%i==0)
-            gcd = i;
-    }
-    x/=gcd;
-    y/=gcd;
-    for(int i=1;;i++){
+    long gcdd=gcd(x,y);
+    x/=gcdd;
+    y/=gcdd;
+    for(long int i=1;;i++){
         if(x*i>a||y*i>b){
             break;
         }
         count++;
     }
+   
     printf("%ld",count);
+}
+
+
+long int gcd (long int a,long int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd (b, a % b);
 }
